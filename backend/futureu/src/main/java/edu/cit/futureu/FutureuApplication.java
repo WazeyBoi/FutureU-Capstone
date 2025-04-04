33ffd9164d716f83354e45a8c7ead3b96d420180
WazeTriesCoding <business.xianprs@gmail.com>
2025-04-04 17:06:16 +0800

    Added Spring Boot Backend Files

---
+++ backend/futureu/src/main/java/edu/cit/futureu/FutureuApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.futureu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureuApplication.class, args);
+	}
+
+}
---
+++ backend/futureu/src/test/java/edu/cit/futureu/FutureuApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.futureu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
