a0aa5602e6e92967d4489a8c53e2839fc14c7668
A RT <aeronraye.tigley@cit.edu>
2025-10-12 21:25:51 +0800

    added cursor-pointer for Profile Pop-up

--- frontend/futureu-capstone/src/components/Profile/CareerInterestProfileSection.jsx
+++ frontend/futureu-capstone/src/components/Profile/CareerInterestProfileSection.jsx
@@ -52,7 +52,7 @@ const CareerInterestProfileSection = ({
             whileHover={{ scale: 1.05, rotate: 1 }}
             whileTap={{ scale: 0.95 }}
             onClick={onEditProfile}
-            className="bg-gradient-to-r from-[#1D63A1] to-[#2B3E4E] hover:from-[#FFB71B] hover:to-[#FF9800] text-white hover:text-[#232D35] px-8 py-4 rounded-2xl font-bold transition-all duration-300 hover:shadow-2xl transform flex items-center shadow-xl border-2 border-white/20"
+            className="bg-gradient-to-r from-[#1D63A1] to-[#2B3E4E] hover:from-[#FFB71B] hover:to-[#FF9800] text-white hover:text-[#232D35] px-8 py-4 rounded-2xl font-bold transition-all duration-300 hover:shadow-2xl transform flex items-center shadow-xl border-2 border-white/20 cursor-pointer"
           >
             <Edit className="w-5 h-5 mr-3" />
             <span>Update Interest Profile </span>
@@ -290,7 +290,7 @@ const CareerInterestProfileSection = ({
                   whileHover={{ scale: 1.05 }}
                   whileTap={{ scale: 0.95 }}
                   onClick={onSetupProfile}
-                  className="w-full md:w-auto bg-gradient-to-r from-[#1D63A1] via-[#2B3E4E] to-[#1D63A1] hover:from-[#FFB71B] hover:via-[#FF9800] hover:to-[#FFB71B] text-white hover:text-[#232D35] px-10 py-4 rounded-2xl font-bold hover:shadow-2xl transform transition-all duration-500 flex items-center justify-center shadow-xl border-2 border-white/20"
+                  className="w-full md:w-auto bg-gradient-to-r from-[#1D63A1] via-[#2B3E4E] to-[#1D63A1] hover:from-[#FFB71B] hover:via-[#FF9800] hover:to-[#FFB71B] text-white hover:text-[#232D35] px-10 py-4 rounded-2xl font-bold hover:shadow-2xl transform transition-all duration-500 flex items-center justify-center shadow-xl border-2 border-white/20 cursor-pointer"
                 >
                   <Sparkles className="w-6 h-6 mr-3" />
                   <span className="text-lg">Set Up Profile Now</span>
